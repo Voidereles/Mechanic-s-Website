6d26992ad5844e860653141ea7ab9a209afdf78e
Maciej Żelazo <voidereles3000@gmail.com>
2019-02-03 18:43:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ $(document).ready(function () {
+     $('#content').load('home.html');
+
+     $('#menu li a').click(function () {
+         var subpage = $(this).attr('href');
+         $('#content').html('Ładuję...');
+         $('#content').load(subpage);
+         return false;
+
+     });
+ });
